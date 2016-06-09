e596616eb81e698a5a0bac2b09edb8412e9797a0
Akshaya Venky <akshaya-venkatesh8@users.noreply.github.com>
2016-06-09 10:03:44 +0530

    Update JsonValidator.java

--- JsonValidator.java
+++ JsonValidator.java
@@ -14,26 +14,26 @@ public class JsonValidator{
      */
     public static void main(String[] args) throws IOException, ProcessingException {
     
-            String json = "{\"address\":{\"name\":\"Lily Joanna\",\"streetnumber\":\"123\",\"streetname\":\"Main Street\",\"secondary address\":\"BLDG 12\",\"city\":\"San Francisco\",\"statecode\":\"CA\",\"zipcode\":\"94111-1234\"}}";
-            String schema = "{\"$schema\": \"http://json-schema.org/draft-04/schema#\",\"address\":{\"type\":\"object\",\"properties\":{\"streetnumber\":{\"type\":\"integer\"},\"streetname\":{\"type\":\"string\",\"minLength\":1,\"maxLength\":34},\"secondaryaddress\":{\"type\":\"string\",\"pattern\": \"APT|BSMT|DEPT|FL|FRNT|HNGR|KEY|LBBY|LOT|LOWR|OFC|PH|PIER|REAR|RM|SIDE|SLIP|SPC|STOP|STE|TRLR|UNIT|UPPR|BLDG|[A-Z]{3} [0-9]{2}([0-9])?\"},\"city\":{\"type\":\"string\",\"minLength\":1,\"maxLength\":24},\"statecode\":{\"type\":\"string\",\"pattern\":\"[A-Z]{2}\"},\"zipcode\":{\"type\":\"string\",\"pattern\":\"[0-9]{5}(-[0-9]{4})?\"}},\"required\":[\"streetnumber\",\"streetname\",\"city\",\"statecode\",\"zipcode\"]}}";
+            File json = new File("address.json");
+            File schema = new File("schema.json");
             JsonValidator jv = new JsonValidator();
             boolean result = jv.validate(json,schema);
             System.out.println(result);
             }
-    public boolean validate(String json,String schema) throws IOException, ProcessingException
+    public boolean validate(File json,File schema) throws IOException, ProcessingException
 { 
     ProcessingReport report = null;
         boolean result = false;
         try{
-            JsonNode schemaNode = JsonLoader.fromString(schema);
-            JsonNode dataNode = JsonLoader.fromString(json);
+            JsonNode schemaNode = JsonLoader.fromFile(schema);
+            JsonNode dataNode = JsonLoader.fromFile(json);
             JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
             JsonSchema sch = factory.getJsonSchema(schemaNode);
             report = sch.validate(dataNode);
         }
         catch (IOException | ProcessingException e)
         {
-            System.out.println("ERROR : "+e.getMessage());
+            System.out.println("Message : "+e.getMessage());
         }
         if(report != null)
         {
